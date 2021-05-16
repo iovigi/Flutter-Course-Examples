b38a41ae919dcf40ca4d95649ed025c5475af614
Iliya Nedelchev <il.nedelchev@gmail.com>
2021-05-16 17:19:15 +0300

    initial commit

---
+++ flutter_first_app/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s is your favorite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(onPressed: null, child: Text('Answer 1')),
+            ElevatedButton(onPressed: null, child: Text('Answer 2')),
+            ElevatedButton(onPressed: null, child: Text('Answer 3')),
+          ],
+        ),
+      ),
+    );
+  }
+}
