6b92a996209a8e9c88d5ba43ece3fb2f314bdf53
Iliya Nedelchev <il.nedelchev@gmail.com>
2021-07-15 00:48:44 +0300

    adding auth screen

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -10,6 +10,7 @@ import 'providers/orders.dart';
 import 'screens/orders_screen.dart';
 import 'screens/user_products_screen.dart';
 import 'screens/edit_product_screen.dart';
+import 'screens/auth-screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
           home: ProductsOverviewScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            AuthScreen.routeName: (ctx) => AuthScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
             OrdersScreen.routeName: (ctx) => OrdersScreen(),
             UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
